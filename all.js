170578456d556eb4a0ef0830e75fbdaa6db0fb30
tiheng0928 <tom830928@gmail.com>
2016-11-20 23:09:55 +0800

    edit all.js

--- all.js
+++ all.js
@@ -1,8 +1,12 @@
-var http = require("http");
-http.createServer(function(request, response) {
-  //console.log("接收到網頁請求！");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(3000);
-//console.log("Server已開啟port: 3000.");
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
