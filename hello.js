2e1cf8dd25c7f9124d7962abe738649d308ce20c
tiheng0928 <tom830928@gmail.com>
2016-11-21 10:17:42 +0800

    edit hello.js

--- hello.js
+++ hello.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express();
 
-//app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
+app.get('/startnode', function (req, res) {
     res.send('Hello World!');
 });
-/*
+
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
-*/
