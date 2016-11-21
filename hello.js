27683eeaad7d7be65340e5aea34a1524b2e432b1
tiheng0928 <tom830928@gmail.com>
2016-11-21 10:08:43 +0800

    edit hello.js

--- hello.js
+++ hello.js
@@ -6,7 +6,8 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-
-//app.listen(app.get('port'), function () {
+/*
+app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
+*/
